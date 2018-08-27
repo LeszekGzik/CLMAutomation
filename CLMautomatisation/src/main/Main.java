699889396ba424c1cdfe20be0406aec672fa1d7d
LeszekGzik <leszgzi982@student.polsl.pl>
2018-08-27 11:36:11 +0200

    JAR test

--- CLMautomatisation/src/main/Main.java
+++ CLMautomatisation/src/main/Main.java
@@ -1,21 +1,19 @@
 package main;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 import javax.xml.stream.XMLStreamException;
 
 import excel.*;
-import inputs.Input;
 import testobjects.*;
 import xml.*;
-/** G³ówna klasa programu. Czyta zawartoœæ properties.xml i wykonuje wszystkie scenariusze testowe zawarte w folderze Scenarios. */
+/** G³ówna klasa programu. Czyta zawartoœæ properties.xml i wykonuje podany w args[0] scenariusz testowy. */
 public class Main {
 	
 	public static void main(String[] args) throws XMLStreamException, IOException {
+		System.out.println("Starting CLMAutomation...");
 		Properties properties = new Properties();
 		int ok = 0, untested = 0, failed = 0;
 		try {
@@ -40,25 +38,5 @@ public class Main {
 			Integer[][] contents = {{ok, failed, untested}};
 			ExcelWriter.appendWithInt(properties.getProperty("xlsoutput"), contents);
 		}
-		
-		/*String scenariofolder = properties.getProperty("scenarios");
-		TestObject.globalInit();
-		Input.setInputWait(Integer.parseInt(properties.getProperty("waitforinput"))/2);
-		File[] folder = new File(scenariofolder).listFiles();
-		int ok = 0, untested = 0, failed = 0;
-		for (File file : folder) {
-			try {
-				TList setup = ExcelReader.readVertical(file.getAbsolutePath());
-				TestScenario scenario = new TestScenario(file.getName(), setup, properties);
-				scenario.executeAllTests();
-				ok = scenario.getNumberOfOkTests();
-				failed = scenario.getNumberOfFailedTests();
-				untested = scenario.getNumberOfUntestedTests();
-			} catch(Exception e) {
-				System.out.println(e);
-			}
-			Integer[][] contents = {{ok, failed, untested}};
-			ExcelWriter.appendWithInt(properties.getProperty("xlsoutput"), contents);
-		}*/
 	}
 }
