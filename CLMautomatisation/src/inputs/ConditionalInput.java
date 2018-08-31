cfd8ffb2a0cab6c83130779448cf7fa853975271
LeszekGzik <leszgzi982@student.polsl.pl>
2018-08-31 11:44:37 +0200

    Dodane wykrywanie braku elementu poprzez IF(xpath)

--- CLMautomatisation/src/inputs/ConditionalInput.java
+++ CLMautomatisation/src/inputs/ConditionalInput.java
@@ -1,28 +1,42 @@
 package inputs;
 
+import org.openqa.selenium.TimeoutException;
+
 import excel.Config;
 import excel.PList;
+import exceptions.TestFailedException;
 import testobjects.TestObject;
 
-/** Ustawia flagê continueStep na false, jeœli zadany parametr jest pusty w testdata. Krok wtedy zostanie zatrzymany. */
+/** Jeœli podano NAME, ustawi flagê continueStep na false, jeœli zadany parametr jest pusty w testdata.
+ *  Jeœli podano XPATH, ustawi flagê continueStep na false, jeœli dany element nie istnieje na stronie.
+ * Krok wtedy zostanie zatrzymany. */
 public class ConditionalInput extends Input {
 	@Override
-	public void fill(PList testdata) throws InterruptedException {
-		
-		PList data;
-		if (name.charAt(0)=='&') {
-			data = TestObject.globaldata;
-			name = name.substring(1);
+	public void fill(PList testdata) throws Exception {
+		if (!name.equals("")) {
+			PList data;
+			if (name.charAt(0)=='&') {
+				data = TestObject.globaldata;
+				name = name.substring(1);
+			}
+			else {
+				data = testdata;
+			}
+			
+			if (data.get(name)==null) {
+				continueStep = false;
+			}
+			else if (data.get(name).equals("")) {
+				continueStep = false;
+			}
 		}
-		else {
-			data = testdata;
-		}
-		
-		if (data.get(name)==null) {
-			continueStep = false;
-		}
-		else if (data.get(name).equals("")) {
-			continueStep = false;
+		if (!xpath.equals("")) {
+			try {
+				super.fill(testdata);
+			}
+			catch (TimeoutException te) {
+				continueStep = false;
+			}
 		}
 	}
 	
--- CLMautomatisation/src/inputs/ConditionalTestInput.java
+++ CLMautomatisation/src/inputs/ConditionalTestInput.java
@@ -1,5 +1,7 @@
 package inputs;
 
+import org.openqa.selenium.TimeoutException;
+
 import excel.Config;
 import excel.PList;
 import testobjects.TestObject;
@@ -8,29 +10,39 @@ import testobjects.TestObject;
 public class ConditionalTestInput extends Input {
 	
 	@Override
-	public void fill(PList testdata) throws InterruptedException {
+	public void fill(PList testdata) throws Exception {
 		PList data;
-		if (name.charAt(0)=='&') {
-			data = TestObject.globaldata;
-			name = name.substring(1);
+		if (!name.equals("")) {
+			if (name.charAt(0)=='&') {
+				data = TestObject.globaldata;
+				name = name.substring(1);
+			}
+			else {
+				data = testdata;
+			}
+			
+			if (data.get(name)==null) {
+				continueStep = false;
+				parent.continueTest = false;
+			}
+			else if (data.get(name).equals("")) {
+				continueStep = false;
+				parent.continueTest = false;
+			}
 		}
-		else {
-			data = testdata;
-		}
-		
-		if (data.get(name)==null) {
-			continueStep = false;
-			parent.continueTest = false;
-		}
-		else if (data.get(name).equals("")) {
-			continueStep = false;
-			parent.continueTest = false;
+		if (!xpath.equals("")) {
+			try {
+				super.fill(testdata);
+			}
+			catch (TimeoutException te) {
+				continueStep = false;
+				parent.continueTest = false;
+			}
 		}
 	}
 	
 	public ConditionalTestInput(Config config, TestObject parent) {
 		super(config, parent);
-		// TODO Auto-generated constructor stub
 	}
 
 }
